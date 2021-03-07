c1d31c8dd14e7a2b0ee7e807a66695fdf8f96999
maks <elfsilon@email.com>
2021-03-07 15:44:39 +0300

    111

--- prime_test.py
+++ prime_test.py
@@ -1,9 +1,10 @@
 import random
 
 # Тест Ферма
-def fermat_test(n, iter_count=100):
-    if n == 2:
-        return True
+def fermat_test(n, iter_count=1000):
+    for p in [2,3,5,7,11,13,17,19,23,29]:
+        if n % p == 0: 
+            return n == p
 
     for _ in range(iter_count):
         x = random.randint(1, n - 1)
@@ -13,20 +14,21 @@ def fermat_test(n, iter_count=100):
 
 
 # Испытание квадратным корнем
-def square_root_test(n):
-    if n == 2:
-        return True
+def square_root_test(n, iter_count=1000):
+    for p in [2,3,5,7,11,13,17,19,23,29]:
+        if n % p == 0: 
+            return n == p
 
-    counter = 0
-    for x in range(1, n):
-        if x**2 % n == 1:
-            counter += 1
-    
-    return counter == 2
+    for _ in range(iter_count):
+        x = random.randint(2, round(n/2))
+        if x*x % n == 1:
+            return False
+
+    return True
 
 
 # Тест Миллера-Рабина
-def miller_rabin_test(n, k=100):
+def miller_rabin_test(n, iter_count=1000):
     if n < 2: 
         return False
 
@@ -34,18 +36,22 @@ def miller_rabin_test(n, k=100):
         if n % p == 0: 
             return n == p
 
-    s, d = 0, n-1
-    while d % 2 == 0:
-        s, d = s+1, d/2
+    s, q = 0, n-1
+    while q % 2 == 0:
+        s, q = s+1, q/2
 
-    for _ in range(k):
-        x = pow(random.randint(2, n-1), int(d), int(n))
+    for _ in range(iter_count):
+        a = random.randint(2, n-1)
+        x = pow(a, int(q), int(n))
+
         if x == 1 or x == n-1: 
             continue
+
         for _ in range(1, s):
-            x = (x * x) % n
-            if x == 1: return False
-            if x == n-1: break
-        else: 
-            return False
+            x = x*x % n
+            if x == 1: 
+                return False
+            if x == n-1:
+                break
+
     return True
